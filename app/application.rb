74646d724155d468c5f80099f91d83751d7426e5
Stela Capsa <stela.capsa@gmail.com>
2020-03-09 04:51:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new 
     t = Time.now.hour
     
-    if t<=12
+    if t <= 12
     resp.write "Good Morning!"
     else
     resp.write "Good Afternoon!"
